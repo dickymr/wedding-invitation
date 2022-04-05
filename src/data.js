f0c0af38c3d1c62ffa1d3f7b1b153a920e352dc5
Dicky Muhamad R <dickymr.dev@gmail.com>
2022-04-06 01:11:43 +0700

    update mock

--- src/data.js
+++ src/data.js
@@ -1,5 +1,8 @@
 export const data = {
   en: {
+    config: {
+      bgm: 'https://cdngarenanow-a.akamaihd.net/gstaticid/BMG/wildwet/sounds/love-song.mp3',
+    },
     header: {
       bgImageLandscape: [
         require('./assets/images/bg-1.jpg'),
@@ -13,13 +16,16 @@ export const data = {
       ],
       bridegroom: 'Dicky & Ira',
       desc: 'ARE GETTING MARRIED',
-      date: 'DECEMBER 12TH, 2020',
-      timestamp: 1607775132000,
+      date: 'APRIL 30TH, 2022',
+      timestamp: 1651331272000,
       ongoing: 'ONGOING',
       days: 'DAYS',
       hours: 'HOURS',
       minutes: 'MINUTES',
       seconds: 'SECONDS',
+      line1: 'Kindly deliver to: Mr. / Mrs. / Ms.',
+      line2: 'We invite you to celebrate our wedding',
+      button: 'Open Invitation',
     },
     navbar: {
       brand: 'D & I',
@@ -53,8 +59,7 @@ export const data = {
           twitter: '',
           instagram: 'https://www.instagram.com/irasq_/',
         },
-        description:
-          'The third daughter of Dedi Rahmat Suardi and Entin Wartini',
+        description: 'The third daughter of Dedi Rahmat Suardi and Entin Wartini',
       },
     },
     events: {
@@ -184,8 +189,7 @@ export const data = {
       ],
     },
     location: {
-      maps:
-        'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d349.96908978547026!2d107.73943603648561!3d-7.073913208208633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c15fe08bdc0f%3A0x8d86ab2ef152e2da!2sGor%20Sabilulungan!5e0!3m2!1sid!2sid!4v1595907723515!5m2!1sid!2sid',
+      maps: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d349.96908978547026!2d107.73943603648561!3d-7.073913208208633!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68c15fe08bdc0f%3A0x8d86ab2ef152e2da!2sGor%20Sabilulungan!5e0!3m2!1sid!2sid!4v1595907723515!5m2!1sid!2sid',
     },
     footer: {
       text: 'Dicky Muhamad R © Copyright 2020, All Rights Reserved.',
